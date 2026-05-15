73f9946f7163ab11b9e83d07338bf058e15e6cd7
vicozero <vicozero559@gmail.com>
2026-05-15 17:28:23 -0600

    Refresh web app cache on deploy

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mga-bitacora-v1'
+const CACHE_NAME = 'mga-bitacora-v2'
 const ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/mga-logo.jfif']
 
 self.addEventListener('install', (event) => {
@@ -23,15 +23,12 @@ self.addEventListener('fetch', (event) => {
     return
   }
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone()
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
-          return response
-        })
-        .catch(() => caches.match('/index.html'))
-    }),
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+        return response
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached ?? caches.match('/index.html'))),
   )
 })
