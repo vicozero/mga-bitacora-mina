534b6b0507e35fe409754429e141e9ccc8dd135f
vicozero <vicozero559@gmail.com>
2026-05-23 06:32:31 -0600

    Rename app to BITACORA MGA

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.mga.bitacoramina',
-  appName: 'MGA Bitacora Mina',
+  appName: 'BITACORA MGA',
   webDir: 'dist'
 };
 
